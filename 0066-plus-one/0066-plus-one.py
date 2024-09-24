0ea1699e0ced2a213b7dd95841933d27feb2bd70
Mabel Mercita <mabelmercita02@gmail.com>
2024-09-24 19:05:50 +0530

    Time: 18 ms (39.75%), Space: 11.6 MB (72.66%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -4,9 +4,7 @@ class Solution(object):
         str1 = ''
         for i in digits:
             str1 = str1+str(i)
-        s = int(str1)+1
-        str2 = str(s)
-        for i in str2:
+        for i in str(int(str1)+1):
             l.append(int(i))
         return l
 
