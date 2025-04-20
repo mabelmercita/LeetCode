cd2d149622c251599fbf2e2c92b24d469d55a70f
Mabel Mercita <mabelmercita02@gmail.com>
2025-04-20 18:18:00 +0530

    Time: 27 ms (27%), Space: 13.6 MB (23.71%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,12 +1,12 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        l1 = list(s)
+        '''l1 = list(s)
         l1.sort()
         l2 = list(t)
-        l2.sort()
+        l2.sort()'''
         '''print(l1)
         print(l2)'''
-        if l1 == l2:
+        if sorted(list(s)) == sorted(list(t)):
             return True
         else:
             return False
