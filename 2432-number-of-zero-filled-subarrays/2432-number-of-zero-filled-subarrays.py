967e5b3051e7f28bfaa4d112c7c5e74674aec791
Mabel Mercita <mabelmercita02@gmail.com>
2025-08-20 18:47:46 +0530

    Time: 27 ms (98.26%), Space: 18.3 MB (66.52%) - LeetHub

---
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def zeroFilledSubarray(self, nums):
+        count = 0
+        streak = 0
+        for num in nums:
+            if num == 0:
+                streak += 1
+                count += streak
+            else:
+                streak = 0
+        return count
+        
