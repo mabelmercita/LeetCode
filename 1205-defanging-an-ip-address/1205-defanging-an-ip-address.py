d771b888d0ac2ab2c3dbca47a058d6986307aabe
Mabel Mercita <mabelmercita02@gmail.com>
2025-11-04 18:04:07 +0530

    Time: 14 ms (66.18%), Space: 12.6 MB (11.42%) - LeetHub

--- 1205-defanging-an-ip-address/1205-defanging-an-ip-address.py
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.py
@@ -1,8 +1,6 @@
 class Solution(object):
     def defangIPaddr(self, address):
         s = ''
-        l = address.split('.')
-        print(l)
         for i in address:
             if i.isdigit():
                 s+=str(i)
