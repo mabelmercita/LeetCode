525eec1d9303bcde9a4808ff30523cc868bb9223
Mabel Mercita <mabelmercita02@gmail.com>
2025-11-19 08:52:04 +0530

    Time: 0 ms (100%), Space: 12.5 MB (18.73%) - LeetHub

---
+++ 0861-flipping-an-image/0861-flipping-an-image.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def flipAndInvertImage(self, image):
+        res = []
+        for i in image:
+            i.reverse()
+            res.append([x ^ 1 for x in i])
+        return res
+        
